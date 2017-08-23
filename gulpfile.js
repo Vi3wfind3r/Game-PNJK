cf0cc972f887cb890322d5aa1b9380464eddf493
Paton Vinal <paton.vinal@gmail.com>
2017-08-23 15:40:14 -0400

    updated gulp server task

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ var keepFiles = false;
 
 gulp.task('serveprod', function() {
   connect.server({
-    root: [SOURCE_PATH],
+    root: SOURCE_PATH,
     port: process.env.PORT || 3000, // localhost:3000
     livereload: false
   });
